3fc1aa5e641b746f8477ffb9ecd7db1efe0d6335
Bennett McDowell <bamcdow@clemson.edu>
2025-09-15 15:01:46 -0400

    Creating about page

---
+++ src/Backend/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, render_template
+#import mysql.connector
+#from mysql.connector import Error
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='../Frontend/static', template_folder='../Frontend/templates')
+CORS(app)
+
+sample_about_data = {
+    "team_number": "Team 03",
+    "version_number": "1",
+    "release_date": "9/16/2025",
+    "product_name": "Driver Incentive Program",
+    "product_description": "product description"
+}
+
+# Sample product data
+product = {
+    "name": "My Product",
+    "description": "This is a description of my awesome product.",
+    "price": "$49.99"
+}
+
+def get_about_data():
+    return sample_about_data
+
+    """
+    try:
+        connection = mysql.connector.connect(
+            host='',
+            database='',
+            user='',
+            password=''
+        )
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT team_number, version_number, release_date, product_name, product_description FROM about_info ORDER BY `index` DESC LIMIT 1;")
+            row = cursor.fetchone()
+            return row
+    except Error as e:
+        print(f"Error: {e}")
+        return None
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+    """
+
+# Serve the about HTML page
+@app.route('/about')
+def about_page():
+    return render_template('about.html', product=sample_about_data)
+
+# Serve the index HTML page
+@app.route('/')
+def home():
+    return render_template('index.html', product=product)
+
+# API endpoint for about data
+@app.route('/api/about', methods=['GET'])
+def about_api():
+    data = get_about_data()
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'No data found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000) 
---
+++ src/Frontend/static/js/about.js
@@ -0,0 +1,20 @@
+fetch('/api/about')
+  .then(response => response.json())
+  .then(data => {
+    if (data.error) {
+      document.getElementById('about-data').innerHTML = '<p>Could not load about data.</p>';
+      return;
+    }
+    document.getElementById('about-data').innerHTML = `
+      <table>
+        <tr><th>Team #</th><td>${data.team_number}</td></tr>
+        <tr><th>Version #</th><td>${data.version_number}</td></tr>
+        <tr><th>Release Date</th><td>${data.release_date}</td></tr>
+        <tr><th>Product Name</th><td>${data.product_name}</td></tr>
+        <tr><th>Product Description</th><td>${data.product_description}</td></tr>
+      </table>
+    `;
+  })
+  .catch(() => {
+    document.getElementById('about-data').innerHTML = '<p>Could not load about data.</p>';
+  });
