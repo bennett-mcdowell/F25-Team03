d8632f662bf0868af78426f7269949501898362c
Devin Evangelista <devinevang@outlook.com>
2025-10-07 13:31:28 -0400

    adds styling changes to about and fixes some navigation issues

--- src/Frontend/static/js/about.js
+++ src/Frontend/static/js/about.js
@@ -5,22 +5,18 @@ fetch('/api/about')
       document.getElementById('about-data').innerHTML = '<p>Could not load about data.</p>';
       return;
     }
-    document.getElementById('about-data').innerHTML = `
-      <table>
-        <tr><th>Team #</th><td>${data.team_number}</td></tr>
-        <tr><th>Version #</th><td>${data.version_number}</td></tr>
-        <tr><th>Release Date</th><td>${data.release_date}</td></tr>
-        <tr><th>Product Name</th><td>${data.product_name}</td></tr>
-        <tr><th>Product Description</th><td>${data.product_description}</td></tr>
-        <tr><th>Contact Details</th><td>${data.contact_details}</td></tr>
-      </table>
+
+    const info = `
+      <div class="about-item"><strong>Team # </strong><span>${data.team_number}</span></div>
+      <div class="about-item"><strong>Version </strong><span>${data.version_number}</span></div>
+      <div class="about-item"><strong>Release Date </strong><span>${data.release_date}</span></div>
+      <div class="about-item"><strong>Product Name </strong><span>${data.product_name}</span></div>
+      <div class="about-item"><strong>Product Description </strong><span>${data.product_description}</span></div>
+      <div class="about-item"><strong>Contact Details </strong><span>${data.contact_details || "Not provided"}</span></div>
     `;
+
+    document.getElementById('about-data').innerHTML = info;
   })
   .catch(() => {
     document.getElementById('about-data').innerHTML = '<p>Could not load about data.</p>';
-  });
-
-  //Home page
-  document.getElementById("homeBtn").addEventListener("click", () => {
-    window.location.href = "/home";
   });
